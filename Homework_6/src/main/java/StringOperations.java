a56e6a710c87c45a6bcaea9c2dfb362d7f5ef5d6
Alexander Pavluchenko <translate86@gmail.com>
2020-02-18 15:52:23 +0300

    Homework_6

---
+++ Homework_6/src/main/java/StringOperations.java
@@ -0,0 +1,21 @@
+public class StringOperations {
+    public int getSum(int a, int b) {
+        return a + b;
+    }
+
+    public int getSubstraction(int a, int b) {
+        return a - b;
+    }
+
+    public int getMultiplication(int a, int b, int c) {
+        return a * b * c;
+    }
+
+    public int getDivision(int a, int b) {
+        return a / b;
+    }
+
+    public double getSquareArea(int a) {
+        return a * a;
+    }
+}
